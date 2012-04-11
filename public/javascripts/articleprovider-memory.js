649d0eb2eef24bff3d6e717d3951a323da6e7d5c
Taylor O'Brien <tmobrien.github@gmail.com>
2012-04-11 14:48:50 -0500

    more stuff for dummy data

--- public/javascripts/articleprovider-memory.js
+++ public/javascripts/articleprovider-memory.js
@@ -47,12 +47,144 @@ new ArticleProvider().save([
 		blurb: 'All purpose text-editing sweetness.',
 		download: {
 			windows: 'http://download.tuxfamily.org/notepadplus/6.1/npp.6.1.Installer.exe',
-			mac: 'http://notepad-plus-plus.org/download/v6.1.html',
-			other: 'http://notepad-plus-plus.org/download/v6.1.html',
+			mac: null,
+			other: null,
 		},
 		icon: '/images/npp.png',
 		info: 'Much better than Notepad.',
-	}
+	},
+	{
+		name : 'Aptana',
+		blurb: 'The Web IDE',
+		download: {
+			windows: '',
+			mac: '',
+			other: '',
+		},
+		icon: '/images/aptana.png',
+		info: 'Built on Eclipse.',
+	},
+	{
+		name : 'Eclipse',
+		blurb: 'All encompassing IDE',
+		download: {
+			windows: '',
+			mac: '',
+			other: '',
+		},
+		icon: '/images/eclipse.png',
+		info: 'Java, C++, etc.',
+	},
+	{
+		name : 'Google Chrome',
+		blurb: 'Super Fast Browser',
+		download: {
+			windows: '',
+			mac: '',
+			other: '',
+		},
+		icon: '/images/chrome.png',
+		info: 'Based on open source Chromium browser',
+	},
+	{
+		name : 'Mozilla Firefox',
+		blurb: 'Fast Browser',
+		download: {
+			windows: '',
+			mac: '',
+			other: '',
+		},
+		icon: '/images/firefox.png',
+		info: 'Either use this or Chrome',
+	},
+	{
+		name : 'Pidgin',
+		blurb: 'Instant Message Client',
+		download: {
+			windows: '',
+			mac: null,
+			other: '',
+		},
+		icon: '/images/pidgin.png',
+		info: 'Customizable, Extensable, Awesome',
+	},
+	{
+		name : '7-zip',
+		blurb: 'Compression',
+		download: {
+			windows:'',
+			mac:'',
+			other:'',
+		},
+		icon: '/images/7zip.png',
+		info: 'Really good compression',
+	},
+	{
+		name :'CCleaner',
+		blurb:'Free up HD space',
+		download: {
+			windows:'',
+			mac:'',
+			other:'',
+		},
+		icon:'/images/ccleaner.png',
+		info:'Removes unnecessary files to free space',
+	},
+	{
+		name :'Evernote',
+		blurb:'Remember Stuff on all the things',
+		download: {
+			windows:'',
+			mac:'',
+			other:'',
+		},
+		icon:'/images/evernote.png',
+		info:'Cross platform lists in the cloud',
+	},
+	{
+		name :'Launchy',
+		blurb:'Launch Apps Fast',
+		download: {
+			windows:'',
+			mac:null,
+			other:null,
+		},
+		icon:'/images/launchy.png',
+		info:'Text based app launcher',
+	},
+	{
+		name :'Windows Grep',
+		blurb:'GUI Grep',
+		download: {
+			windows:'',
+			mac:null,
+			other:null,
+		},
+		icon:'/images/wingrep.png',
+		info:'GUI Grep tool for Windows',
+	},
+	{
+		name :'WinMerge',
+		blurb:'Pretty Diffs',
+		download: {
+			windows:'',
+			mac:null,
+			other:null,
+		},
+		icon:'/images/winmerge.png',
+		info:'Diff all the things',
+	},
+	{
+		name :'VLC',
+		blurb:'Play all the things!',
+		download: {
+			windows:'',
+			mac:'',
+			other:'',
+		},
+		icon:'/images/vlc.png',
+		info:'Media player that plays everything',
+	},
 ], function(error, articles) {
 });
 
